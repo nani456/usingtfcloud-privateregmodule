79973feb6f074f96a11837cbd4ddec3ea47413c1
Nagini Kandula <nagini.kandula@gmail.com>
2026-02-18 20:40:18 +0530

    sett22222222

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 module "reuse_mod" {
   source  = "app.terraform.io/nagini-own/tf-module-s3-local/self"
 <<<<<<< HEAD
-  version = "1.0.4"
+  version = "1.0.5"
 =======
   version = "1.0.4"
 >>>>>>> a5606f07830c7372ef3e5a73a1117f43a4f6f99e
