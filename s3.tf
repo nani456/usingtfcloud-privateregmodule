1bb5a46015e969733ed6287e33b2c8a7be417a46
Nagini Kandula <nagini.kandula@gmail.com>
2026-02-18 21:39:47 +0530

    new luck

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 module "reuse_mod" {
   source  = "app.terraform.io/nagini-own/tf-module-s3-local/self"
-  version = "1.0.1"
+  version = "1.0.2"
   bucket-name = var.s3_buc_name
 tags = var.tags
 }
